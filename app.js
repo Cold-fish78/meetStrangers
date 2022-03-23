826287b26a417e9937ee718200d16097d8e77ec3
Cold-fish78 <pubg69086@gmail.com>
2022-03-23 15:12:26 +0530

    addded socket.io

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const exp = require('constants');
 const express = require('express');
+const req = require('express/lib/request');
 const http = require('http');
+const { Socket } = require('socket.io');
 const PORT = process.env.PORT || 6002;
 
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + 'public/index.html');
+});
+
+let connectedPeers = [];
+
+io.on('connection',(socket)=>{
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers);
+    socket.on('disconnect',()=>{
+        ;
+        newConnectecPeers = connectedPeers.filter((peerSocketId)=>{
+            peerSocketId !== socket.id;
+        });
+        connectedPeers = newConnectecPeers;
+        console.log(connectedPeers);
+console.log('user disconnected')
+    });
 });
 
 server.listen(PORT, function (err) {
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const socket = io('/');
+
+
+
+socket.on('connect',()=>{
+    console.log('successfully connected to web socket server');
+    console.log(socket.id);
+});
